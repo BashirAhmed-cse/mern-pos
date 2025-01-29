fc186770fcbf13adbc3fe8e1ac05d60d44a82b14
bashirahmed-cse <bashirahmed.cse@gmail.com>
2025-01-29 18:14:00 +0600

    recent orders start

---
+++ pos-client/src/components/home/RecentOrders.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RecentOrders = () => {
+  return (
+    <div className='px-8 mt-6'>
+        <div className='bg-[#1a1a1a] w-full h-[450px] rounded-lg'>
+            <div className='flex justify-between items-center px-6 py-4'>
+                <h1>Recent Orders</h1>
+                <a href='' className='text-[#025cca] text-sm font-semibold'>View all</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default RecentOrders
