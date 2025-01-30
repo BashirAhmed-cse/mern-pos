c6fb690f455bde3f2e2126bb6792dee8129e335f
Bashir <bashirahmed.cse@gmail.com>
2025-01-30 17:04:07 +0600

    order list and recent order

--- pos-client/src/components/home/RecentOrders.jsx
+++ pos-client/src/components/home/RecentOrders.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import OrderList from "./OrderList";
 
 const RecentOrders = () => {
   return (
-    <div className='px-8 mt-6'>
-        <div className='bg-[#1a1a1a] w-full h-[450px] rounded-lg'>
-            <div className='flex justify-between items-center px-6 py-4'>
-                <h1>Recent Orders</h1>
-                <a href='' className='text-[#025cca] text-sm font-semibold'>View all</a>
-            </div>
+    <div className="px-8 mt-6">
+      <div className="bg-[#1a1a1a] w-full h-[450px] rounded-lg">
+        <div className="flex justify-between items-center px-6 py-4">
+          <h1 className="text-[#f5f5f5] text-xl font-semibold tracking-wide">
+            Recent Orders
+          </h1>
+          <a href="" className="text-[#025cca] text-sm font-semibold">
+            View all
+          </a>
         </div>
+        <div className="flex items-center gap-4 bg-[#1f1f1f] rounded-[20px] px-4 py-2 mx-6">
+          <FaSearch className="text-[#f5f5f5]" />
+          <input
+            type="text"
+            placeholder="Search recent orders"
+            className="bg-transparent w-full outline-none text-[#f5f5f5] placeholder:text-[#ababab]"
+          />
+        </div>
+        {/* Order list */}
+        <div className="mt-4 px-6 overflow-y-scroll h-[300px] scrollbar-hide">
+        <OrderList />
+        <OrderList />
+        <OrderList />
+        <OrderList />
+        <OrderList />
+        <OrderList />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecentOrders
+export default RecentOrders;
