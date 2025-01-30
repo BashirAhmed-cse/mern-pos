d481a592c9f619b60fee39c8abd9678f710afbdc
Bashir <bashirahmed.cse@gmail.com>
2025-01-30 23:47:34 +0600

    update table ui

---
+++ pos-client/src/components/tables/TableCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TableCard = () => {
+  return (
+    <div className='w-[400px] bg-[#262626] p-4 rounded-lg mb-4'>
+        
+    </div>
+  )
+}
+
+export default TableCard
