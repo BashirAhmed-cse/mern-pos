7041c393e23956338d30220df9a887ad87babb3c
bashirahmed-cse <bashirahmed.cse@gmail.com>
2025-02-01 18:52:50 +0600

    bill complete

--- pos-client/src/components/menu/Bill.jsx
+++ pos-client/src/components/menu/Bill.jsx
@@ -12,11 +12,15 @@ const Bill = () => {
     <>
       <div className="flex items-center justify-between px-5 mt-2">
         <p className="text-xs text-[#ababab] font-medium mt-2">Items({cartData.length})</p>
-        <h1 className="text-[#f5f5f5] text-md font-bold">${total}</h1>
+        <h1 className="text-[#f5f5f5] text-md font-bold">${total.toFixed(2)}</h1>
       </div>
       <div className="flex items-center justify-between px-5 mt-2">
         <p className="text-xs text-[#ababab] font-medium mt-2">Tax({taxRate}%)</p>
-        <h1 className="text-[#f5f5f5] text-md font-bold">${tax}</h1>
+        <h1 className="text-[#f5f5f5] text-md font-bold">${tax.toFixed(2)}</h1>
+      </div>
+      <div className="flex items-center justify-between px-5 mt-2">
+        <p className="text-xs text-[#ababab] font-medium mt-2">Total With Tax</p>
+        <h1 className="text-[#f5f5f5] text-md font-bold">${totalPriceWithTax.toFixed(2)}</h1>
       </div>
       <div className="flex items-center gap-3 px-5 mt-4">
         <button className="bg-[#1f1f1f] px-4 py-3 w-full rounded-lg text-[#ababab] font-semibold">
