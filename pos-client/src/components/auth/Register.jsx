8cdc3d6c0a79f9dcb40eb6ae47607f25edb64167
Bashir <bashirahmed.cse@gmail.com>
2025-02-17 23:36:25 +0600

    register and login ui complete

---
+++ pos-client/src/components/auth/Register.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+    role: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleRoleSelection = (selectedRole) => {
+    setFormData({ ...formData, role: selectedRole });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label className="block text-[#ababab] mb-2 text-md font-medium">
+            Employee Name
+          </label>
+          <div className="flex items-center rounded-lg p-5 px-4 bg-[#1f1f1f]">
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              placeholder="Enter employee name"
+              className="bg-transparent flex-1 text-white focus:outline-none"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <label className="block text-[#ababab] mb-2 mt-3 text-md font-medium">
+            Employee Email
+          </label>
+          <div className="flex items-center rounded-lg p-5 px-4 bg-[#1f1f1f]">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Enter employee email"
+              className="bg-transparent flex-1 text-white focus:outline-none"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <label className="block text-[#ababab] mb-2 mt-3 text-md font-medium">
+            Employee Phone
+          </label>
+          <div className="flex items-center rounded-lg p-5 px-4 bg-[#1f1f1f]">
+            <input
+              type="tel"
+              name="phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+              placeholder="Enter employee phone"
+              pattern="[0-9]{10,15}"
+              className="bg-transparent flex-1 text-white focus:outline-none"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <label className="block text-[#ababab] mb-2 mt-3 text-md font-medium">
+            Password
+          </label>
+          <div className="flex items-center rounded-lg p-5 px-4 bg-[#1f1f1f]">
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              placeholder="Enter password"
+              className="bg-transparent flex-1 text-white focus:outline-none"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <label className="block text-[#ababab] mb-2 mt-3 text-md font-medium">
+            Choose your role
+          </label>
+          <div className="flex items-center gap-3 mt-4">
+            {["Waiter", "Cashier", "Admin"].map((role) => {
+              return (
+                <button
+                  key={role}
+                  type="button"
+                  onClick={() => handleRoleSelection(role)}
+                  className={`px-4 py-3 w-full rounded-lg text-[#ababab] cursor-pointer transition-all ${
+                    formData.role === role ? "bg-yellow-400 text-gray-900" : "bg-[#1f1f1f]"
+                  }`}
+                >
+                  {role}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="w-full mt-6 py-3 text-lg bg-yellow-400 rounded-lg text-gray-900 font-bold"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
